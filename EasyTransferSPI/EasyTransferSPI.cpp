b123c36633fde1d1ac20a5ea43ee0ee282ae9fd7
Eugene <eugene.prout1@gmail.com>
2022-06-18 10:50:43 +0100

    feat: add beginMaster methods

--- EasyTransferSPI/EasyTransferSPI.cpp
+++ EasyTransferSPI/EasyTransferSPI.cpp
@@ -1,1 +1,19 @@
 #include "EasyTransferSPI.h"
+
+void EasyTransferSPI::beginMaster(uint8_t *structPtr, uint8_t structSize, SPI *theSPI)
+{
+    address = structPtr;
+    size = structSize;
+    _spi = theSPI;
+    _spi.begin()
+    _spi.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0));
+}
+
+void EasyTransferSPI::beginMasterbeginMaster(uint8_t *structPtr, uint8_t structSize, SPI *theSPI, SPISettings customSettings)
+{
+    address = structPtr;
+    size = structSize;
+    _spi = theSPI;
+    _spi.begin()
+    _spi.beginTransaction(customSettings);
+}
